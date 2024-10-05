6622d494476d3f3e5c9474de769020cfc4e28607
Jai Prakash sharma <sjaiprakash457@gmail.com>
2024-10-05 14:45:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,19 +9,26 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const url = `https://dev.to/api/articles/me`;
+  try {
+      const res = await fetch(url, {
+          method: 'GET',
+          headers: {
+              'Accept': 'application/vnd.forem.api-v1+json',
+              'api-key': process.env.DEV_TO_API_KEY
+          }
+      });
+      if (!res.ok) {
+          throw new Error('Failed to fetch data');
+      }
+      const textData = await res.text();
+      const data = JSON.parse(textData); 
+      return data;
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
+  } catch (error) {
+      throw error; 
   }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
+}
 export default async function Home() {
   const blogs = await getData();
 
